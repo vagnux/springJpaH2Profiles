96608d9eb3605543773da45709a53d9a1b8d3e3e
Vagner Rodrigues <vagner.rodrigues@tradeupgroup.com>
2020-02-13 14:30:09 -0300

    oneToMany part1

--- src/main/java/br/com/yatode/springJpa/entities/User.java
+++ src/main/java/br/com/yatode/springJpa/entities/User.java
@@ -1,15 +1,20 @@
 package br.com.yatode.springJpa.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -22,6 +27,9 @@ public class User implements Serializable {
 	private String phone;
 	@JsonIgnore
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -64,6 +72,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	public User() {
 
 	}
@@ -101,5 +113,7 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 
 }
