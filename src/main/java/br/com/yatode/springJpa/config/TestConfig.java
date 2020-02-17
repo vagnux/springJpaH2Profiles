2eab148d643ee4c43b01ad9cbe2cd5493e7c0d7c
Vagner Rodrigues <vagner.rodrigues@tradeupgroup.com>
2020-02-17 13:39:11 -0300

    category

--- src/main/java/br/com/yatode/springJpa/config/TestConfig.java
+++ src/main/java/br/com/yatode/springJpa/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.yatode.springJpa.entities.Category;
 import br.com.yatode.springJpa.entities.Order;
 import br.com.yatode.springJpa.entities.User;
 import br.com.yatode.springJpa.entities.enums.OrderStatus;
+import br.com.yatode.springJpa.repositories.CategoryRepository;
 import br.com.yatode.springJpa.repositories.OrderRepository;
 import br.com.yatode.springJpa.repositories.UserRepository;
 
@@ -24,10 +26,18 @@ public class TestConfig  implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -35,7 +45,7 @@ public class TestConfig  implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
-		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
